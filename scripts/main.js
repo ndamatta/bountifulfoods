1c834edc4b0dfdbb18292d59cd95ceaf1b384bcc
ndamatta <damatta.natanael@gmail.com>
2023-07-05 17:57:07 -0700

    added lastmodified and copyright

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,22 @@ function toggleMenu() {
 
 const x = document.getElementById("hamburgerBtn")
 x.onclick = toggleMenu;
+
+// COPYRIGHT AND LAST MODIFIED
+let lastModified = document.querySelector(".lastmodified")
+let lastModified2 = document.querySelector(".lastmodified2")
+let currentYear = document.querySelector("#currentyear")
+
+const lastModifiedOptions = {
+    day: "numeric",
+    month: "numeric",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+};
+const currentYearOptions = {
+    year: "numeric",
+};
+lastModified.textContent = ` Last Modified: ${new Date(document.lastModified).toLocaleDateString("en-US", lastModifiedOptions)}`;
+currentYear.textContent = `${new Date().toLocaleDateString("en-US", currentYearOptions)}`
