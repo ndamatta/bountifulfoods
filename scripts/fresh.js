f55b11d665ca6a7e91a626e958c1bc0729175fa9
ndamatta <damatta.natanael@gmail.com>
2023-07-12 18:57:59 -0700

    worked on build my drink function and style

--- scripts/fresh.js
+++ scripts/fresh.js
@@ -21,3 +21,93 @@ fetch(url)
     .catch(error => {
       console.error('Error fetching fruit data:', error);
     });
+
+// DISPLAY NUTRITION
+function calculateTotalNutrition() {
+  const ingredientList1 = document.querySelector('#ingredientList1');
+  const ingredientList2 = document.querySelector('#ingredientList2');
+  const ingredientList3 = document.querySelector('#ingredientList3');
+
+  const selectedFruits = [
+    ingredientList1.options[ingredientList1.selectedIndex].text,
+    ingredientList2.options[ingredientList2.selectedIndex].text,
+    ingredientList3.options[ingredientList3.selectedIndex].text
+  ];
+
+  const fname = document.getElementById('fname').value;
+  const email = document.getElementById('email').value;
+  const phone = document.getElementById('phone').value;
+
+  // if (fname.trim() === '' || email.trim() === '' || phone.trim() === '') {
+  //   return;
+  // }
+
+  fetch(url)
+    .then(response => response.json())
+    .then(fruits => {
+      const selectedFruitsObj = fruits.filter(fruit => selectedFruits.includes(fruit.name));
+
+      const totalNutrition = selectedFruitsObj.reduce((sum, fruit) => {
+        const { carbohydrates, protein, fat, calories, sugar } = fruit.nutritions;
+        return {
+          carbohydrates: sum.carbohydrates + carbohydrates,
+          protein: sum.protein + protein,
+          fat: sum.fat + fat,
+          calories: sum.calories + calories,
+          sugar: sum.sugar + sugar
+        };
+      }, { carbohydrates: 0, protein: 0, fat: 0, calories: 0, sugar: 0 });
+
+      const nutritionDiv = document.createElement('div');
+      nutritionDiv.setAttribute("class", "nutritionDiv");
+
+      nutritionDiv.innerHTML = 
+      `
+      <h2>Your drink's info</h2>
+      <table>
+        <tr>
+          <th>Nutrient</th>
+          <th>Amount</th>
+        </tr>
+        <tr>
+          <td>Carbos</td>
+          <td>${totalNutrition.carbohydrates.toFixed(1)}g</td>
+        </tr>
+        <tr>
+          <td>Protein</td>
+          <td>${totalNutrition.protein.toFixed(1)}g</td>
+        </tr>
+        <tr>
+          <td>Fat</td>
+          <td>${totalNutrition.fat.toFixed(1)}g</td>
+        </tr>
+        <tr>
+          <td>Calories</td>
+          <td>${totalNutrition.calories.toFixed(1)}g</td>
+        </tr>
+        <tr>
+          <td>Sugar</td>
+          <td>${totalNutrition.sugar.toFixed(1)}g</td>
+        </tr>
+      </table>
+      `;
+
+      const resultContainer = document.getElementById('result-container');
+      resultContainer.innerHTML = '';
+      resultContainer.appendChild(nutritionDiv);
+    })
+    .catch(error => {
+      console.error('Error fetching fruit data:', error);
+    });
+}
+
+
+
+// <h2>Your drink's nutritionals:</h2>
+//       <div class="nutri-info">
+//         <p>Carbohydrates: ${totalNutrition.carbohydrates}g</p>
+//         <p>Protein: ${totalNutrition.protein}g</p>
+//         <p>Fat: ${totalNutrition.fat}g</p>
+//         <p>Calories: ${totalNutrition.calories}g</p>
+//         <p>Sugar: ${totalNutrition.sugar}g</p>
+//       </div>
