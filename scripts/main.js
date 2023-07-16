e7245828570e0bd164f6b6a167d7c702c135aeb8
ndamatta <damatta.natanael@gmail.com>
2023-07-15 18:27:07 -0700

    worked on home drinks card design and lazyload

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,39 @@ function toggleMenu() {
 const x = document.getElementById("hamburgerBtn")
 if (x) {x.onclick = toggleMenu;}
 
+// LAZY LOAD
+const imagesToLoad = document.querySelectorAll(".home-main img[data-src]");
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages = (image) => {
+image.setAttribute("src", image.getAttribute("data-src"));
+image.onload = () => {image.removeAttribute("data-src");};
+};
+
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+        if (item.isIntersecting) {
+            loadImages(item.target);
+            observer.unobserve(item.target);
+        }
+        });
+    }, imgOptions);
+
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    });
+} 
+else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+};
+
 // COPYRIGHT AND LAST MODIFIED
 let lastModified = document.querySelector(".lastmodified")
 let currentYear = document.querySelector("#currentyear")
